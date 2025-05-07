fd96d8f2bdcfb7fde9d2dd3bb17cbbd041a146e0
Penkov Maksym AKA Maksyn006 <ipz231_pmo@student.ztu.edu.ua>
2025-05-07 20:50:57 +0300

    Implement visitor pattern

---
+++ lab4/VisitorTask/AbstractNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorTask;
+
+abstract class AbstractNode : INode
+{
+    private Dictionary<string, List<EventCallback>> eventListeners = [];
+
+    public abstract string InnerHTML { get; }
+    public abstract string OuterHTML { get; }
+
+    public void AddEventListener(string eventType, EventCallback callback)
+    {
+        if (!eventListeners.ContainsKey(eventType))
+            eventListeners[eventType] = [];
+        eventListeners[eventType].Add(callback);
+    }
+
+    public void TriggerEvent(string eventType)
+    {
+        if (eventListeners.TryGetValue(eventType, out var listeners))
+        {
+            foreach (var callback in listeners)
+                callback(this);
+        }
+    }
+}
--- lab4/VisitorTask/Program.cs
+++ lab4/VisitorTask/Program.cs
@@ -1,3 +1,22 @@
 ﻿using VisitorTask;
 
+var div = new ElementNode("div");
 
+div.AddEventListener("click", (sender) =>
+{
+    if (sender is ElementNode elementNode)
+        Console.WriteLine($"Clicked on: <{elementNode.TagName}> element");
+    else if (sender is TextNode textNode)
+        Console.WriteLine($"Clicked on: \"{textNode.InnerHTML}\" text");
+});
+
+div.AddEventListener("mouseover", (sender) =>
+{
+    if (sender is ElementNode elementNode)
+        Console.WriteLine($"Mouse over: <{elementNode.TagName}> element");
+    else if (sender is TextNode textNode)
+        Console.WriteLine($"Mouse over: \"{textNode.InnerHTML}\" text");
+});
+
+div.TriggerEvent("click");
+div.TriggerEvent("mouseover");
