c627756bbd2d90f7d157decd0c1bb9cf3dda51cc
Penkov Maksym AKA Maksyn006 <ipz231_pmo@student.ztu.edu.ua>
2025-05-07 22:53:46 +0300

    Strategy Task

---
+++ lab4/StrategyTask/ImageNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace StrategyTask;
+
+class ImageNode : INode
+{
+    public string Href { get; }
+    private readonly IImageLoadingStrategy _strategy;
+
+    public ImageNode(string href)
+    {
+        Href = href;
+
+        if (Regex.IsMatch(href, @"^http"))
+            _strategy = new NetworkImageLoadingStrategy();
+        else
+            _strategy = new FileImageLoadingStrategy();
+
+    }
+
+    public string InnerHTML => string.Empty;
+
+    public string OuterHTML => _strategy.Load(Href);
+}
---
+++ lab4/StrategyTask/Strategies.cs
@@ -0,0 +1,23 @@
+﻿namespace StrategyTask;
+
+
+interface IImageLoadingStrategy
+{
+    string Load(string href);
+}
+
+class FileImageLoadingStrategy : IImageLoadingStrategy
+{
+    public string Load(string href)
+    {
+        return $"<img src=\"file://{href}\" />";
+    }
+}
+
+class NetworkImageLoadingStrategy : IImageLoadingStrategy
+{
+    public string Load(string href)
+    {
+        return $"<img src=\"{href}\" />";
+    }
+}
