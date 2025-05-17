6183a2dbfa4c1e27d3ba26d5ab42fde3b7918ff0
Penkov Maksym AKA Maksyn006 <ipz231_pmo@student.ztu.edu.ua>
2025-05-17 14:55:19 +0300

    Implement iterator pattern

--- mkr1/LightHTML/Program.cs
+++ mkr1/LightHTML/Program.cs
@@ -3,19 +3,35 @@
 using LightHTML;
 
 ElementNode container = new("div");
+container.CssClasses.Add("container");
+container.CssClasses.Add("d-flex");
 
-ButtonNode btn = new();
-container.Children.Add(btn);
-btn.AddEventListener("click", new SaveCommand());
-btn.AddEventListener("mouseenter", new HoverCommand());
-btn.AddEventListener("mouseleave", () => { Console.WriteLine("Mouse leaved btn"); });
+for (int i = 0; i < 4; i++)
+{
+    ButtonNode btn = new();
+    container.Children.Add(btn);
+    btn.AddEventListener("click", () => { Console.WriteLine($"Redirecting to page /items/item-{i}/"); });
+    btn.CssClasses.Add("p-3");
+    btn.CssClasses.Add("rounded");
+    btn.CssClasses.Add("bg-secondary");
+    if (i == 2) btn.CssClasses.Add("bg-warning");
+    btn.AddEventListener("mouseenter", new HoverCommand());
 
-TextNode txt = new("Click on me!");
-btn.Children.Add(txt);
+    TextNode txt = new($"Click on me {i}!");
+    btn.Children.Add(txt);
+}
+
+
+
 
 string document = container.OuterHTML;
 Console.WriteLine(document);
 
-btn.TriggerEvent("click");
-btn.TriggerEvent("mouseenter");
-btn.TriggerEvent("mouseleave");
+foreach(var item in container)
+{
+    Console.WriteLine(item.OuterHTML);
+}
+
+AbstractNode node = (AbstractNode)container.First(n => n is AbstractNode);
+node.TriggerEvent("mouseenter");
+node.TriggerEvent("click");
