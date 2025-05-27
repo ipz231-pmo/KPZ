443312cac4d1aa60b6a109a872285a341918f8c3
ipz232_boo <ipz232_boo@student.ztu.edu.ua>
2025-05-27 20:20:20 +0300

    Fix: Replace DbContext Singleton with proper disposal pattern

--- lab6/lab6/Models/MineSweeperDbContext.cs
+++ lab6/lab6/Models/MineSweeperDbContext.cs
@@ -11,21 +11,9 @@ namespace lab6.Models;
 public class MineSweeperDbContext : DbContext
 {
     public DbSet<User> Users => Set<User>();
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MinesweeperDb;Trusted_Connection=True;");
     }
-
-    private MineSweeperDbContext() : base() { }
-
-    public static MineSweeperDbContext Instance
-    { 
-        get
-        {
-            instance ??= new();
-            return instance;
-        } 
-    }
-    private static MineSweeperDbContext? instance;
-
 }
--- lab6/lab6/ViewModels/RegisterVM.cs
+++ lab6/lab6/ViewModels/RegisterVM.cs
@@ -11,66 +11,65 @@ namespace lab6.ViewModels;
 
 class RegisterVM : VMBase
 {
-	private string login = "";
-	private string password = "";
-	private string passwordConfirm = "";
+    private string login = "";
+    private string password = "";
+    private string passwordConfirm = "";
 
-	public RelayCommand RegisterCmd { get; set; }
+    public RelayCommand RegisterCmd { get; set; }
 
-	public EventHandler<RegisterEventsArgs>? RegisterHndl { get; set; }
-	public EventHandler? AuthHndl { get; set; }
+    public EventHandler<RegisterEventsArgs>? RegisterHndl { get; set; }
+    public EventHandler? AuthHndl { get; set; }
 
-	public RegisterVM()
-	{
-		RegisterCmd = new(register, canRegister);
-	}
-
-
-	private void register(object? param)
-	{
-		var db = MineSweeperDbContext.Instance;
-		User? user = db.Users.FirstOrDefault(u => u.Login == Login);
-		if (user != null)
-		{
-			Login = "";
-			Password = "";
-			PasswordConfirm = "";
-			MessageBox.Show("User login are taken");
-			return;
-        }
-		user = new User() { Login = Login, Password = Password };
-		db.Users.Add(user);
-		db.SaveChanges();
-		RegisterHndl?.Invoke(this, new RegisterEventsArgs(user));
-	}
-	private bool canRegister(object? param)
-	{
-		return Login.Length >= 4 && Password.Length >= 8 && Password == PasswordConfirm;
-	}
-
-    public string Login
-	{
-		get => login; 
-		set { login = value; NotifyOnPropertyChanged(); }
-	}
-	public string Password
-	{
-		get { return password; }
-		set { password = value; NotifyOnPropertyChanged(); }
-	}
-	public string PasswordConfirm
-	{
-		get { return passwordConfirm; }
-		set { passwordConfirm = value; NotifyOnPropertyChanged(); }
-	}
-
-	public class RegisterEventsArgs : EventArgs
-	{
-        public User User { get; set; }
-		public RegisterEventsArgs(User user)
-		{
-			User = user;
-		}
+    public RegisterVM()
+    {
+        RegisterCmd = new(register, canRegister);
     }
 
+    private void register(object? param)
+    {
+        using var db = new MineSweeperDbContext();
+        User? user = db.Users.FirstOrDefault(u => u.Login == Login);
+        if (user != null)
+        {
+            Login = "";
+            Password = "";
+            PasswordConfirm = "";
+            MessageBox.Show("User login are taken");
+            return;
+        }
+        user = new User() { Login = Login, Password = Password };
+        db.Users.Add(user);
+        db.SaveChanges();
+        RegisterHndl?.Invoke(this, new RegisterEventsArgs(user));
+    }
+
+    private bool canRegister(object? param)
+    {
+        return Login.Length >= 4 && Password.Length >= 8 && Password == PasswordConfirm;
+    }
+
+    public string Login
+    {
+        get => login;
+        set { login = value; NotifyOnPropertyChanged(); }
+    }
+    public string Password
+    {
+        get { return password; }
+        set { password = value; NotifyOnPropertyChanged(); }
+    }
+    public string PasswordConfirm
+    {
+        get { return passwordConfirm; }
+        set { passwordConfirm = value; NotifyOnPropertyChanged(); }
+    }
+
+    public class RegisterEventsArgs : EventArgs
+    {
+        public User User { get; set; }
+        public RegisterEventsArgs(User user)
+        {
+            User = user;
+        }
+    }
 }
