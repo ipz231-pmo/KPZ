a1276ecdea9331bb377497149a8b0556595f7c9f
Kotiuk Sofiia <ipz231_ksv@student.ztu.edu.ua>
2025-05-28 11:46:59 +0300

    add IMessageService in MainVM file

--- lab6/lab6/ViewModels/MainVM.cs
+++ lab6/lab6/ViewModels/MainVM.cs
@@ -1,76 +1,79 @@
-﻿using lab6.Models;
+﻿using lab6.Core;
+using lab6.Core.Services;
+using lab6.Models;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace lab6.ViewModels;
-
-class MainVM : Core.VMBase
+namespace lab6.ViewModels
 {
-	private object currentVM;
-	private User? currentUser;
-
-	public MainVM()
-	{
-		setAuthVM();
-	}
-	private void setAuthVM()
-	{
-        var vm = new AuthVM();
-        vm.AuthHndl += (sender, e) => {
-            currentUser = e.User;
-            setMenuVM();
-        };
-		vm.RegisterHndl += (sender, e) =>
-		{
-            setRegisterVM();
-		};
-        CurrentVM = vm;
-    }
-
-    private void setRegisterVM()
+    public class MainVM : VMBase
     {
-        var vm = new RegisterVM();
-        vm.RegisterHndl += (sender, e) => {
-            currentUser = e.User;
-            setMenuVM();
-        };
-        vm.AuthHndl += (sender, e) => {
-            setAuthVM();
-        };
-        CurrentVM = vm;
-    }
+        private object _currentVM;
+        private User? _currentUser;
 
-    private void setMenuVM()
-	{
-		var vm = new MenuVM();
-		vm.LogoutHndl += (sender, e) =>
-		{
-			currentUser = null;
-			setAuthVM();
-		};
-        vm.ExitHndl += (sender, e) => { App.Current.MainWindow.Close(); };
-        vm.StartHndl += (sender, e) => setGameVM();
-		CurrentVM = vm;
-	}
-    private void setGameVM()
-    {
-        var vm = new GameVM();
-        vm.EndHndl += (sender, e) => { setMenuVM(); };
-        CurrentVM = vm;
-    }
-
-    public object CurrentVM
-    {
-        get => currentVM;
-        set
+        public MainVM()
         {
-            currentVM = value;
-            NotifyOnPropertyChanged();
+            SetAuthVM();
+        }
+
+        private void SetAuthVM()
+        {
+            // Тепер передаємо і UserService, і MessageService
+            var vm = new AuthVM(new UserService(), new MessageService());
+            vm.AuthHndl += (sender, e) =>
+            {
+                _currentUser = e.User;
+                SetMenuVM();
+            };
+            vm.RegisterHndl += (sender, e) =>
+            {
+                SetRegisterVM();
+            };
+            CurrentVM = vm;
+        }
+
+        private void SetRegisterVM()
+        {
+            var vm = new RegisterVM(new UserService(), new MessageService());
+            vm.RegisterHndl += (sender, e) =>
+            {
+                _currentUser = e.User;
+                SetMenuVM();
+            };
+            vm.AuthHndl += (sender, e) =>
+            {
+                SetAuthVM();
+            };
+            CurrentVM = vm;
+        }
+
+        private void SetMenuVM()
+        {
+            var vm = new MenuVM();
+            vm.LogoutHndl += (sender, e) =>
+            {
+                _currentUser = null;
+                SetAuthVM();
+            };
+            vm.ExitHndl += (sender, e) => { App.Current.MainWindow.Close(); };
+            vm.StartHndl += (sender, e) => SetGameVM();
+            CurrentVM = vm;
+        }
+
+        private void SetGameVM()
+        {
+            var vm = new GameVM();
+            vm.EndHndl += (sender, e) => { SetMenuVM(); };
+            CurrentVM = vm;
+        }
+
+        public object CurrentVM
+        {
+            get => _currentVM;
+            set
+            {
+                _currentVM = value;
+                NotifyOnPropertyChanged();
+            }
         }
     }
 }
